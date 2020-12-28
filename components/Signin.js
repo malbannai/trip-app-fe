4f11d9df8885f938167bf4c3bc56e8f67b7df3a3
malbannai <moudhi.albannai@gamil.com>
2020-12-28 20:19:04 +0300

    Profile details is here

--- components/Signin.js
+++ components/Signin.js
@@ -1,9 +1,9 @@
 import { Button, Container, Form, Input, Item, Label } from "native-base";
 import React, { useState } from "react";
 import { StyleSheet, Text } from "react-native";
-
 import { AuthOther } from "../styles";
 import authStore from "../stores/authStore";
+import usersStore from "../stores/usersStore";
 import { observer } from "mobx-react";
 
 const Signin = ({ navigation }) => {
@@ -14,10 +14,10 @@ const Signin = ({ navigation }) => {
 
   const handleSubmit = async () => {
     await authStore.signin(user);
-
+    const owner = usersStore.users.find((u) => user.username === u.username);
     // if (authStore.user) navigation.navigate("TripList"); //change to profile
     //   if (authStore.user) navigation.navigate("CreateTrip");
-    if (authStore.user) navigation.navigate("Profile", { profileOwner: user }); //change to profil
+    if (authStore.user) navigation.navigate("Profile", { profileOwner: owner }); //change to profil
   };
   return (
     <Container>
