4e76a9dab006d850d5ea144c20816b42996a76fa
malbannai <moudhi.albannai@gamil.com>
2020-12-28 16:21:33 +0300

    ActionBarImage is now an observer

--- components/ActionBarImage.js
+++ components/ActionBarImage.js
@@ -2,6 +2,7 @@ import React from "react";
 import authStore from "../stores/authStore";
 import { useNavigation } from "@react-navigation/native";
 import { UserIcon } from "../styles";
+import { observer } from "mobx-react";
 
 const ActionBarImage = () => {
   const navigation = useNavigation();
@@ -20,4 +21,4 @@ const ActionBarImage = () => {
   );
 };
 
-export default ActionBarImage;
+export default observer(ActionBarImage);
