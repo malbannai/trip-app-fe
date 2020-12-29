dbd13d18bdc6f39826c4c5e2126e3474f9993079
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-12-29 15:03:00 +0300

    removed double userstore import in signin

--- components/Signin.js
+++ components/Signin.js
@@ -1,9 +1,9 @@
 import { Button, Container, Form, Input, Item, Label } from "native-base";
 import React, { useState } from "react";
 import { StyleSheet, Text } from "react-native";
+
 import { AuthOther } from "../styles";
 import authStore from "../stores/authStore";
-import usersStore from "../stores/usersStore";
 import { observer } from "mobx-react";
 import usersStore from "../stores/usersStore";
 
@@ -16,15 +16,13 @@ const Signin = ({ navigation }) => {
   const handleSubmit = async () => {
     await authStore.signin(user);
     const owner = usersStore.users.find((u) => user.username === u.username);
- 
+
     console.log("from sgin in>>", owner);
     // if (authStore.user) navigation.navigate("TripList"); //change to profile
     //   if (authStore.user) navigation.navigate("CreateTrip");
-     
- 
+
     if (authStore.user.id !== 0)
       navigation.navigate("Profile", { profileOwner: owner }); //change to profil
- 
   };
   return (
     <Container>
